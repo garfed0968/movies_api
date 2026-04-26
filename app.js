cb2cb4dfb26d87cd4570a15c4546b6aa6253eefc
garfed0968 <garfed0968@gmail.com>
2026-04-25 19:10:43 -1000

    Update app.js
    Added DELETE and UPDATE function

--- app.js
+++ app.js
@@ -44,7 +44,45 @@ const server = http.createServer(async (req, res) => {
             res.end(JSON.stringify(newMovie));
         });
     }
-   
+    else if (req.url.startsWith('/api/movies/') && req.method === 'PUT') {
+
+    const id = parseInt(req.url.split('/')[3]);
+
+    let body = '';
+
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+
+    req.on('end', async () => {
+        const movie = JSON.parse(body);
+        movie.id = id;
+
+        const updated = await updateMovie(movie);
+
+        if (updated) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(updated));
+        } else {
+            res.writeHead(404);
+            res.end('Movie not found');
+        }
+    });
+}
+   else if (req.url.startsWith('/api/movies/') && req.method === 'DELETE') {
+
+    const id = parseInt(req.url.split('/')[3]);
+
+    const deleted = await deleteMovie(id);
+
+    if (deleted) {
+        res.writeHead(200);
+        res.end('Movie deleted successfully');
+    } else {
+        res.writeHead(404);
+        res.end('Movie not found');
+    }
+}
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Route not found');
@@ -58,6 +96,31 @@ const addMovie =async(movie) => {
     movies.push(movie);
     await fs.promises.writeFile(moviesDataPath,JSON.stringify(movies, null,2));
     return movie;
+};
+const updateMovie = async (movie) =>{
+     const data = await fs.promises.readFile(moviesDataPath, 'utf-8');
+    const movies = JSON.parse(data);
+    const index = movies.findIndex(m => m.id === movie.id);
+    if (index !== -1){
+        movies[index] = movie;
+     await fs.promises.writeFile(moviesDataPath,JSON.stringify(movies, null,2));
+    return movie; 
+    }
+    return null;
+};
+const deleteMovie = async (id) => {
+    const data = await fs.promises.readFile(moviesDataPath, 'utf-8');
+    const movies = JSON.parse(data);
+
+    const newMovies = movies.filter(m => m.id !== id);
+
+    if (movies.length === newMovies.length) {
+        return false;
+    }
+
+    await fs.promises.writeFile(moviesDataPath, JSON.stringify(newMovies, null, 2));
+
+    return true;
 };
     
 const PORT = 3000;
